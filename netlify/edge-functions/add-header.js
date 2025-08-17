f86bcf41dc0ca1afc890e70c2756764747b5b603
pirate <3ld3r.p1r4t3@gmail.com>
2025-08-17 19:21:41 -0300

    Fix: Corrige estrutura de arquivos para a Edge Function

--- netlify/edge-functions/add-header.js
+++ netlify/edge-functions/add-header.js
@@ -1,10 +1,10 @@
 export default async (request, context) => {
-    // Pega a resposta da página que o servidor ia enviar
+    
     const response = await context.next();
 
-    // Adiciona nosso cabeçalho customizado à resposta
+    
     response.headers.set("X-Maestro-Bypass-Route", "/core_override/7A3F.html");
 
-    // Retorna a resposta modificada
+    
     return response;
 };
